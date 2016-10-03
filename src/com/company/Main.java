6d32932c154cd0e37d1518eac328827ade4f9bba
Steven Burris <stevenburris.jr@gmail.com>
2016-10-03 11:40:32 -0400

    Created home and login page, Working on get/post methods;

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,37 @@
 package com.company;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    HashMap m = new HashMap();
+                    m.put("name","Alice");
+                    return new ModelAndView(m,"home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+        Spark.get(
+                "/login",
+                (request, response) -> {
+                    return new ModelAndView(null, "login.html");
+                },
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                    response.redirect("/");
+                    return null;
+                }
+        );
 
     }
 }
